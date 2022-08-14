6b4172c239593b6792470c305e2f5a588fd87108
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-14 00:08:07 -0600

    final commit of the night

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,26 @@ var eventContainer = $(".description");
 var displayDate = $("#currentDay");
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 var saveButton = $("#saveButton");
-
+var formCell = $(".form-control");
+var time = 0
 displayDate.text(today) 
 console.log(today);
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 function saveEvent() {
-    eventContainer = "";
+    textarea.textContent = "";
+//   element   
+    console.log(saveEvent);
    
     //if save button is clicked, it will store what is in the eventContainer to local storage
     if (saveButton.clicked) {
-        localStorage.setItem(eventContainer);
+        localStorage.setItem();
     }
 }
- if (eventContainer < today) {
+ if (time < today) {
+    time++
     //if statement saying that if the time is less than the current date/time, it is in the past, if greater than the today time, it is future
-    
+
  }
 // saveButton.addEventListener("click", saveEvent); 
 //This is how I am going to store the planner details in local storage
