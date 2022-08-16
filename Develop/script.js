d4b4901d8a19bc410880b0b09882a8d20d736ff0
madrodgerflynn <madrodgerflynn@gmail.com>
2022-08-15 18:07:38 -0600

    Just putting the finishing touches on it

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,63 @@
 var eventContainer = $(".description");
 var displayDate = $("#currentDay");
-var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+var today = moment().format("MMMM Do YYYY, h:mm:ss a");
 var saveButton = $(".saveBtn");
 var formCell = $(".form-control");
-var time = 0
-var textarea = $("textarea");
-displayDate.text(today) 
+var now = moment().format("HH");
+var textareas = $("textarea");
+displayDate.text(today);
 var row = $(".row");
+var storageDataArr = [];
+var hours = $(".hour")
+var dataHour = $("#10am");
+var divHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
 
-row = []
-// console.log(today);
-// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
 
 //I need to take what is in the textarea and make it into a string using
-JSON.stringify(textarea);
 
 // Event handler for button
-saveButton.on("click", function() {
+saveButton.on("click", function () {
+    // console.log("press");
+    // console.log(textareas);
+    //This is how I am going to store the planner details in local storage
+    storageDataArr = [];
+    for (var i = 0; i < textareas.length; i++) {
+        
+        var textValue = textareas[i].value;
+        console.log(textValue);
+        storageDataArr.push(textValue);
+        
+    }
+    localStorage.setItem("textarea", JSON.stringify(storageDataArr));
+
+});
+
+$( document ).ready(function() {
+  storageDataArr = JSON.parse(localstorage.getItem("textarea"));
     
+});
+function pageLoad() {
+    console.log(moment().format("HH"));
+    console.log(divHours[1]);
+    for (var i = 0; i < divHours.length; ++i) {
+    if (divHours[i] < now ) {
+        eventContainer.
+        console.log("past");
+        } else if (divHours[i] == now) {
+            console.log("present");
+         } else if (divHours[i] > now) {
+            console.log("future");
+         }
+    }
 
-    console.log("press");
-    console.log(textarea);
-    localStorage.setItem('textarea', textarea);
-}); 
+} 
 
-//This is how I am going to store the planner details in local storage
-// 
+//put into init fx
+
+    
+    pageLoad();
+
+    console.log();
+    console.log(now)
+    // console.log(dataHour.getAttribute("data-dayTime"));
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Day-Planner
+
+This is an interactive Day-Planner that will save your data to local storage and indicate whether its past/future/present
+
+## Installation 
+
+Installation should be straightforward
+
+## Usage
+
+- Enter the event you would like to store within the time block
+- Press Save to save your event to the local storage
+- Watch as your time slots change from green to red to white as time progresses
+
+## ScreenShot
+
